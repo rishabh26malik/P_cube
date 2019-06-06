5ea1c1344bca69ad8f8823d0ae1f9342dd81c31f
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-06-06 18:47:01 +0530

    Create 1.cpp
    pattern

---
+++ pattern_and_series_level_2/1.cpp
@@ -0,0 +1,32 @@
+/*
+	n=5	m=4 (# of rows)
+
+	*****	
+	 *****
+	  *****
+	   *****
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int n,m,space=0;			
+	cout<<"enter n , m : ";
+	cin>>n>>m;
+	int i,j;
+	for(i=1;i<=m;i++)
+	{
+		for(j=0;j<space;j++)
+			cout<<" ";
+		for(j=1;j<=n;j++)
+		{
+			cout<<"*";
+		}
+		space++;
+		cout<<endl;
+	}
+	return 0; 
+} 
