9761ae079086a59ca8ae42cf72cd032b47a4d1b1
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-06-09 20:52:51 +0530

    Stock buy and sell 2

--- Tests/Stock_buy_and_sell_2.cpp
+++ Tests/Stock_buy_and_sell_2.cpp
@@ -1,3 +1,40 @@
+/*
+The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so that in between those days your profit is maximum.
+
+Input Format
+
+First line of each test case contains an integer value N denoting the number of days, followed by an array of stock prices of N days.
+
+Constraints
+
+1 <= T <= 100
+
+2 <= N <= 103
+
+0 <= Ai <= 104
+
+Output Format
+
+For each testcase, output all the days with profit in a single line. And if there is no profit then print "No Profit"
+
+Sample Input 0
+
+7
+100 180 260 310 40 535 695
+Sample Output 0
+
+(0,3) (4,6)
+Explanation 0
+
+We can buy stock on day 0, and sell it on 3rd day, which will give us maximum profit.
+
+Similar for purchasing on day 4 and selling on day 6
+
+Submissions: 116
+Max Score: 100
+
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
